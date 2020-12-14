c58a187b959adaea07c9079fec4f522c30878ed8
Zenkov Artem <azenkv@gmail.com>
2020-12-14 17:05:10 +0700

    error fix

--- deals_backend/utils.py
+++ deals_backend/utils.py
@@ -17,10 +17,12 @@ def int_validator(min_value=None, max_value=None):
 
         if max_digits and len(s)>max_digits:
             raise ValueError(f'Too big number {s}')
+
         n = int(s)
-        if min_value and n < min_value:
+
+        if not min_value is None and n < min_value:
             raise ValueError(f'Minimum value is {min_value}. Number:{s}')
-        if max_value and n > max_value:
+        if not max_value is None and n > max_value:
             raise ValueError(f'Maximum value is {max_value} Number: {s}')
 
         return n
